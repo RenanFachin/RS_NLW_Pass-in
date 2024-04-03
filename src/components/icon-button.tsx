27d5cc248f4a87ac7502227d06b4024b0afbc86b
Renan <renan.fachin@hotmail.com>
2024-04-03 11:42:41 -0300

    style: Adiciona estilização condicional aos botões da paginação

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface IconButtonProps extends ComponentProps<'button'> {
   children: ReactNode
@@ -12,11 +13,16 @@ export function IconButton({
 }: IconButtonProps) {
   return (
     <button
-      className={
-        isTransparent
-          ? 'bg-black/20 border border-white/10 rounded-md p-1.5'
-          : 'bg-white/10 border border-white/10 rounded-md p-1.5'
-      }
+      // className={
+      //   isTransparent
+      //     ? 'bg-black/20 '
+      //     : ' border border-white/10 rounded-md p-1.5'
+      // }
+      className={twMerge(
+        'border border-white/10 rounded-md p-1.5',
+        isTransparent ? 'bg-black/20' : 'bg-white/10',
+        props.disabled ? 'opacity-50' : null,
+      )}
       {...props}
     >
       {children}
