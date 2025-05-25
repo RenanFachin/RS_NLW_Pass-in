ea95bc829b5e5a6cee4928e52f4638681a25fbd5
Renan <renan.fachin@hotmail.com>
2025-05-25 16:03:45 -0300

    style: testando o fluxo de ci

--- src/components/table/table-header.tsx
+++ src/components/table/table-header.tsx
@@ -3,5 +3,5 @@ import { ComponentProps } from 'react'
 interface TableHeaderProps extends ComponentProps<'th'> { }
 
 export function TableHeader({ ...props }: TableHeaderProps) {
-  return <th className="py-3 px-4 text-sm font-semibold text-left" {...props} />
+  return <th className="py-3 px-4 text-sm font-semibold text-left bg-zinc-800" {...props} />
 }
